491eb3e7743713fdcbf349ab185a1fdbf9ba3461
Michael Chang <thenewme91@gmail.com>
2013-09-04 23:08:07 -0400

    Use a pretty date for posts.

--- lib/default.rb
+++ lib/default.rb
@@ -5,3 +5,11 @@ include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Tagging
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::LinkTo
+
+module PostHelper
+    def get_pretty_date(post)
+        attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
+    end
+end
+
+include PostHelper
